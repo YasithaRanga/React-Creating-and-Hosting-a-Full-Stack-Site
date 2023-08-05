d4b907c5ca42567e6043114d7482e644c63c42c4
Yasitha Ranga <yasitharanga1@gmail.com>
2023-08-06 02:37:09 +0530

    Created Comment List Component and added it to the article page

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -29,6 +30,7 @@ const ArticlePage = () => {
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments} />
         </>
     )
 }
