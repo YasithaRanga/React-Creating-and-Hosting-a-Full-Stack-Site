94e3dce1753fe2cb20e69c169e80045f5a9de0cb
Yasitha Ranga <yasitharanga1@gmail.com>
2023-08-06 02:22:30 +0530

    Added Axios and loaded data from the server

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
+import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
     const { articleId } = useParams();
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`http://localhost:9000/api/articles/${articleId}`);
+            setArticleInfo(response.data);
+        }
+        loadArticleInfo();
+    }, [])
+
     const article = articles.find(article => article.name === articleId);
 
     if (!article) {
@@ -13,6 +25,7 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s).</p>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
