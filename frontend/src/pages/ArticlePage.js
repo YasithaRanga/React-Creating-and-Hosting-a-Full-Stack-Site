c38d09911c8f332d61ffd55cf22342d1bce5d9e0
Yasitha Ranga <yasitharanga1@gmail.com>
2023-08-12 19:37:53 +0530

    Added Firebase auth to login and create account

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -5,11 +5,14 @@ import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 import CommentsList from '../components/CommentsList';
 import AddCommentForm from '../components/AddCommentForm';
+import useUser from '../hooks/useUser';
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
-
     const { articleId } = useParams();
+
+    const { user, isLoading } = useUser();
+
     useEffect(() => {
         const loadArticleInfo = async () => {
             const response = await axios.get(`http://localhost:9000/api/articles/${articleId}`);
@@ -33,13 +36,19 @@ const ArticlePage = () => {
         <>
             <h1>{article.title}</h1>
             <div className='upvotes-section'>
-                <button onClick={addUpvote}>Upvote</button>
+                {user
+                    ? <button onClick={addUpvote}>Upvote</button>
+                    : <button >Log in to upvote</button>
+                }
                 <p>This article has {articleInfo.upvotes} upvote(s).</p>
             </div>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
-            <AddCommentForm articleId={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            {user
+                ? <AddCommentForm articleId={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+                : <button >Log in to add comment</button>
+            }
             <CommentsList comments={articleInfo.comments} />
         </>
     )
