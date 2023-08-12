e6e58c504836bc7e4f23924c5cc02fb529b2eb75
Yasitha Ranga <yasitharanga1@gmail.com>
2023-08-12 10:42:42 +0530

    Completed Frontend

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import articles from './article-content';
 import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -19,6 +20,11 @@ const ArticlePage = () => {
 
     const article = articles.find(article => article.name === articleId);
 
+    const addUpvote = async () => {
+        const response = await axios.put(`http://localhost:9000/api/articles/${articleId}/upvote`);
+        setArticleInfo(response.data);
+    }
+
     if (!article) {
         return <NotFoundPage />;
     }
@@ -26,10 +32,14 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
-            <p>This article has {articleInfo.upvotes} upvote(s).</p>
+            <div className='upvotes-section'>
+                <button onClick={addUpvote}>Upvote</button>
+                <p>This article has {articleInfo.upvotes} upvote(s).</p>
+            </div>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
+            <AddCommentForm articleId={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
             <CommentsList comments={articleInfo.comments} />
         </>
     )
